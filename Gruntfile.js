89a2dca71ee7db2d422419defed6597dea1f2562
Luke <lukehaas7@gmail.com>
2014-05-05 16:20:59 +0100

    update

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+          files: [{
+              expand: true,
+              cwd: 'jade/',
+              src: ['**/*.jade'],
+              dest: 'html/',
+              ext: '.html'
+          }]
+      }
+    },
+    less: {
+      src: {
+        expand: true,
+        cwd: 'less/',
+        src: ['**/*.less'],
+        dest: 'css/',
+        ext: '.css'
+      }
+    },
+    watch: {
+      files: ['less/**/*.less','jade/**/*.jade'],
+      tasks: ['default']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less','jade','watch']);
+
+};
