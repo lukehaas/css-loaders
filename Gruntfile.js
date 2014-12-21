814409f9bd99dc5b58a6927c125c60e956e66e3a
Nikita Bayev <drugoi.nik@gmail.com>
2014-12-21 08:48:33 +0600

    Fixed loading of autoprefixer task

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jade');
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  
+  grunt.loadNpmTasks('grunt-autoprefixer');
 
   grunt.loadNpmTasks('grunt-contrib-watch');
 
